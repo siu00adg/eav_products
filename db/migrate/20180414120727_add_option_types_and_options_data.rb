193eb3b945735e7a74f5182b964d8038613d549c
Alex Green <alex.green@greenandclark.co.uk>
2018-04-14 18:13:45 +0100

    Migration tweak.

--- db/migrate/20180414120727_add_option_types_and_options_data.rb
+++ db/migrate/20180414120727_add_option_types_and_options_data.rb
@@ -1,26 +1,15 @@
 class AddOptionTypesAndOptionsData < ActiveRecord::Migration[5.1]
   def change
     reversible do |dir|
+      option_types = ["varchar","integer","decimal","datetime","multiselect"]
       dir.up do
-        option_type = OptionType.create(:name => 'varchar')
-        Option.create(:name => 'description', :option_type => option_type)
-        option_type = OptionType.create(:name => 'integer')
-        Option.create(:name => 'position', :option_type => option_type)
-        option_type = OptionType.create(:name => 'decimal')
-        Option.create(:name => 'price', :option_type => option_type)
-        option_type = OptionType.create(:name => 'datetime')
-        Option.create(:name => 'expiry', :option_type => option_type)
-        option_type = OptionType.create(:name => 'multiselect')
-        Option.create(:name => 'size', :option_type => option_type)
+        option_types.each do |ot|
+          OptionType.create(:name => ot)
+        end
       end
       dir.down do
-        option_types = ["varchar","integer","decimal","datetime","multiselect"]
-        options = ["description","position","price","expiry","size"]
         option_types.each do |ot|
           OptionType.find_by_name(ot).delete if OptionType.find_by_name(ot)
-        end
-        options.each do |o|
-          Option.find_by_name(o).delete if Option.find_by_name(o)
         end
       end
     end
