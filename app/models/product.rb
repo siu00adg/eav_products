387aeaa4f0311aae7ca07f4c208f9d865d7ffd4a
Alex Green <alex.green@greenandclark.co.uk>
2018-04-21 23:07:02 +0100

    Adding sort to the filter.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,24 +3,51 @@ class Product < ApplicationRecord
   belongs_to :product_type
 
   OPERATORS = {:equal => "=", :like => "LIKE", :gt => ">", :lt => "<"}
-
-  def self.sort
-    #need to work out how to sort by an option
-  end
+  SORT_OPERATORS = {:asc => "ASC", :desc => "DESC"}
 
   # Strangely it can be quicker to chain filters together rather than add all
   # options to the filter hash at once. This is not always the case, if you
   # are expecting a very large number of results then chaining can be slower.
+  #
   # Best performace seems to be when you arrange the filters in order of number
   # records eliminated first and eliminate a lot of records.
+  #
   # Further benefits come when you use chained filters and then modify
   # one of the filters as all filters to the left of the one that was changed
   # will be cached in mysql.
-  def self.filter(filter_hash = {})
+  #
+  # Using sorting on top of the filter will slow the query down a bit as
+  # we lose the sort order and have to reinstate it in the second mysql request.
+  #
+  # Can't use .last on resulting relation if sorting.
+  #
+
+  def self.filter(filter_hash = {}, sort_hash = {})
+    # Need to add filtering and sorting of columns on the products tables too
+
     start = Time.now
     if !filter_hash.empty?
       filter_array = []
-      types_array = []
+      sort_array = []
+      type_array = []
+      option_array = []
+
+      sort_hash.each do |key, value|
+        option = Option.find_by_name(key.to_s)
+        if !option
+          # Option doesn't exist so return nil
+          return nil
+        end
+
+        if !SORT_OPERATORS[value]
+          # Operator doesn't exist so return nil
+          return nil
+        end
+        sort_array << [option, SORT_OPERATORS[value]]
+
+        option_array << option
+        type_array << option.option_type.name
+      end
 
       filter_hash.each do |key, value|
         option = Option.find_by_name(key.to_s)
@@ -37,45 +64,47 @@ class Product < ApplicationRecord
         else
           filter_array << [option, OPERATORS[:equal], value]
         end
-        types_array << option.option_type.name
+        option_array << option
+        type_array << option.option_type.name
       end
 
-      types_array = types_array.uniq
+      type_array = type_array.uniq
+      option_array = option_array.uniq
 
       type_sql = ""
-      types_array.each_with_index do |type, index|
+      type_array.each_with_index do |type, index|
         type_sql << "`#{type}_values`.`data` "
         type_sql << "AS `#{type}_data` "
-        type_sql << ", " if index != types_array.size - 1
+        type_sql << ", " if index != type_array.size - 1
       end
 
       join_sql = ""
-      types_array.each do |type|
+      type_array.each do |type|
         join_sql << "LEFT JOIN `#{type}_values` "
         join_sql << "ON `values`.`valuable_id` = `#{type}_values`.`id` "
         join_sql << "AND `values`.`valuable_type` = '#{type.capitalize}Value' "
       end
 
       where_option_sql = "WHERE `values`.`option_id` IN ("
-      filter_array.each_with_index do |filter, index|
-        where_option_sql << "#{filter[0].id}"
-        where_option_sql << "," if index != filter_array.size - 1
+      option_array.each_with_index do |option, index|
+        where_option_sql << "#{option.id}"
+        where_option_sql << "," if index != option_array.size - 1
       end
       where_option_sql << ")"
 
       case_sql = ""
-      filter_array.each_with_index do |filter, index|
-        case_sql << "CASE WHEN `p`.`option_id` = #{filter[0].id} "
-        case_sql << "THEN `p`.`#{filter[0].option_type.name}_data` "
-        case_sql << "END AS `#{filter[0].name}` "
-        case_sql << ", " if index != filter_array.size - 1
+      option_array.each_with_index do |option, index|
+        case_sql << "CASE WHEN `p`.`option_id` = #{option.id} "
+        case_sql << "THEN `p`.`#{option.option_type.name}_data` "
+        case_sql << "END AS `#{option.name}` "
+        case_sql << ", " if index != option_array.size - 1
       end
 
       max_sql = ""
-      filter_array.each_with_index do |filter, index|
-        max_sql << "MAX(IFNULL(`p2`.`#{filter[0].name}`,0)) "
-        max_sql << "AS `#{filter[0].name}` "
-        max_sql << ", " if index != filter_array.size - 1
+      option_array.each_with_index do |option, index|
+        max_sql << "MAX(IFNULL(`p2`.`#{option.name}`,0)) "
+        max_sql << "AS `#{option.name}` "
+        max_sql << ", " if index != option_array.size - 1
       end
 
       # This is for filtering by an attribute on the product record
@@ -88,6 +117,15 @@ class Product < ApplicationRecord
         where_value = "%" + where_value + "%" if filter[1] == "LIKE"
         where_sql << "(`#{filter[0].name}` #{filter[1]} '#{where_value}') "
         where_sql << "AND " if index != filter_array.size - 1
+      end
+
+      sort_sql = ""
+      if !sort_hash.empty?
+        sort_sql << "ORDER BY "
+        sort_array.each_with_index do |sort, index|
+          sort_sql << "`#{sort[0].name}` #{sort[1]} "
+          sort_sql << ", " if index != filter_array.size - 1
+        end
       end
 
       # Possible future optimisation:
@@ -113,9 +151,15 @@ class Product < ApplicationRecord
                                #{where_product_attribute_sql}) AS `p`) AS `p2`
                GROUP  BY `p2`.`id`) AS `p3`
         #{where_sql}
+        #{sort_sql}
       SQL
       results_array = self.find_by_sql(query)
-      result = self.where(id: results_array.map(&:id))
+      if sort_hash.empty?
+        result = self.where(id: results_array.map(&:id))
+      else
+        result = self.where(id: results_array.map(&:id))
+                     .order("field(id, #{results_array.map(&:id).join(', ')})")
+      end
       logger.info("Filter results: #{results_array.count} record(s)")
       logger.info("Filter time taken: #{Time.now - start} seconds")
       result
